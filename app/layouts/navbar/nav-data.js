2390ecad66fe7065131bc71ebda99ff7a02ec597
abdfikih <abdulfikih863@gmail.com>
2024-11-20 09:40:11 +0700

    fresh commit

---
+++ app/layouts/navbar/nav-data.js
@@ -0,0 +1,24 @@
+import config from '~/config.json';
+
+export const navLinks = [
+  {
+    label: 'Projects',
+    pathname: '/#project-1',
+  },
+  {
+    label: 'Teams',
+    pathname: '/#details',
+  },
+  {
+    label: 'Contact',
+    pathname: '/contact',
+  },
+];
+
+export const socialLinks = [
+  {
+    label: 'Github',
+    url: `https://github.com/}`,
+    icon: 'github',
+  },
+];
